e8e1ddecd99b966b38b61a017f597f9272226ab4
arunraj <arunrajmla@gmail.com>
2025-04-26 13:19:30 +0530

    implemented next function

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
+app.use((req,res,next)=>{
+    console.log('Yes we did it !!')
+    next();
+})
+
+app.use(()=>{
+    console.log('Yes we did it again!!')
+})
+
 const server = http.createServer(app)
 server.listen(3000)
