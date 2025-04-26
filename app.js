7b5bdbd74f672f745afb78989d0ca18bcf68fe40
arunraj <arunrajmla@gmail.com>
2025-04-26 13:45:28 +0530

    we can use HTTP SERVER

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const http = require('http')
+// const http = require('http')
 const express = require('express')
 const app = express()
 
 app.use((req,res,next)=>{
-    console.log(req.url);
-    console.log('Yes we did it !!')
+    // console.log(req.url);
+    // console.log('Yes we did it !!')
     next();
 })
 
-app.use(()=>{
-    console.log('Yes we did it again!!')
+app.use((req,res)=>{
+    res.send('<h1> Hai </h1>')
+    // console.log('Yes we did it again!!')
 })
 
-const server = http.createServer(app)
-server.listen(3000)
+// const server = http.createServer(app)
+// server.listen(3000)
+
+app.listen(3000)
